6d74ec865df22a51049c51d69f275b5985052fd5
Vivek Brohma <vivek.brohma@zalando.de>
2017-10-22 16:20:51 +0200

    Separating out scss into css file

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,21 @@ const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const devtool = process.env.NODE_ENV === 'production' ? 'sourcemap' : false;
+
+const extractSass = new ExtractTextPlugin({
+    filename: "[name].css",
+    // disable: process.env.NODE_ENV === "development"
+});
 
 module.exports = {
     entry: {
         popup: './src/index.js',
         options: './src/options.js',
+        popupStyle: './src/index.scss',
+        optionsStyle: './src/options.scss',
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -28,20 +36,28 @@ module.exports = {
                         presets: ['env', 'react']
                     }
                 }
+            },
+            {
+                test: /\.scss$/,
+                use: extractSass.extract({
+                    use: [{loader: 'css-loader'}, {loader: 'sass-loader'}],
+                    fallback: 'style-loader'
+                })
             }
         ]
     },
     plugins: [
+        extractSass,
         new HtmlWebpackPlugin({
             title: 'App',
             chunks: ['popup'],
-            template: './src/templates/index.template.html',
+            template: './src/templates/popup.template.html',
             filename: 'popup.html',
         }),
         new HtmlWebpackPlugin({
             title: 'App - Options',
             chunks: ['options'],
-            template: './src/templates/index.template.html',
+            template: './src/templates/options.template.html',
             filename: 'options.html',
         }),
         new CopyWebpackPlugin([
